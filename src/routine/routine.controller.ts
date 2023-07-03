70713dabafc1005cfb1b1dd60e0d0fa9c35cf666
2heeesss <hohooodo@gmail.com>
2023-07-03 15:37:00 +0900

    [feat] #8 add create routine API endpoint

--- src/routine/routine.controller.ts
+++ src/routine/routine.controller.ts
@@ -1,10 +1,24 @@
-import { Query, Controller, Get, Param } from '@nestjs/common';
+import {
+  Query,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { RoutineServiceImpl } from './services/routine.service';
 import { UserIdDTO, DateStringDTO } from './dto/findRoutinesByDate.dto';
+import { CreateRoutineDTO } from './dto/createRoutine.dto';
+import { UserService } from '../user/service/user.service';
 
 @Controller('api')
 export class RoutineController {
-  constructor(private readonly routineService: RoutineServiceImpl) {}
+  constructor(
+    private readonly routineService: RoutineServiceImpl,
+    private readonly userService: UserService,
+  ) {}
 
   @Get('user/:userId/routines')
   findRoutinesByDate(
@@ -19,5 +33,35 @@ export class RoutineController {
     } catch (error) {
       console.error(error);
     }
+  }
+
+  @Post('user/:userId/routines')
+  async createRoutine(
+    @Param() userIdDTO: UserIdDTO,
+    @Body() createRoutineDTO: CreateRoutineDTO,
+  ) {
+    const hasUser = await this.userService.getUserByUserId(userIdDTO.userId);
+
+    if (!hasUser) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: 'User with provided ID not found',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const newRoutine = await this.routineService.createRoutine(
+      userIdDTO.userId,
+      createRoutineDTO.title,
+      createRoutineDTO.type,
+      createRoutineDTO.daysOfWeek,
+      createRoutineDTO.goal,
+    );
+
+    return {
+      routine: newRoutine,
+    };
   }
 }
